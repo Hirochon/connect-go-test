e7e9a375c589807c8698dc55991f717c0e5e96a7
Hirochon <heacet43@gmail.com>
2023-03-29 02:39:57 +0900

    add: main.go

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,7 @@
 module github.com/Hirochon/connect-go-test/server
 
 go 1.20
+
+require golang.org/x/net v0.8.0
+
+require golang.org/x/text v0.8.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	"github.com/Hirochon/connect-go-test/server/protocolbuffers/greet/v1/greetv1connect"
+)
+
+func server() http.Handler {
+	mux := http.NewServeMux()
+	mux.Handle(greetv1connect.NewGreetServiceHandler(nil))
+	return mux
+}
 
 func main() {
-	fmt.Println("Hello World")
+	mux := server()
+	http.ListenAndServe(
+		":5050",
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
 }
